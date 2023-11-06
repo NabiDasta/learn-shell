9587e7bdec278a7a50bce05b97a648aaea25b998
Nabi <nabisaheb7780@gmail.com>
2023-11-06 15:46:31 +0530

    Expense poject setup

--- loop.sh
+++ loop.sh
@@ -1,5 +1,12 @@
+#while and for loop
+
 a=10
 while [ "$a" -gt 0 ]; do
   echo nabi
   a=$((a-1))
 done
+
+for sample in pynaple biscute chocalate ; do
+  echo sample name - $smaple
+  sleep 1
+  done
